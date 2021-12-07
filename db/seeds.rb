56afad854394c03d45c34b9ef8cbf8896f9a74d2
robinlashae1 <robinlashae1@gmail.com>
2021-12-07 15:11:48 -0500

    seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Arbiter.destroy_all
+Team.destroy_all
+Court.destroy_all
+Arbiter.reset_pk_sequence
+Team.reset_pk_sequence
+Court.reset_pk_sequence
+
+bobby= Arbiter.create(name: "bobby", rate: 5, email: "bob@yahoo.com", phone_number: "8122230816")
+ale= Arbiter.create(name: "ale", rate: 60, email: "ale@gmail.com", phone_number: "3030300307")
+will= Arbiter.create(name: "will", rate: 30, email: "will@gmail.com", phone_number: "2150300307")    
+puts "Arbiter added"
+bullhogs= Team.create(name: "bullhogs", email: "bullhogs@gmail.com")
+walnuts=Team.create(name: "walnuts", email: "walnuts@gmail.com")
+wonkies= Team.create(name: "wonkies", email: "wonkies@gmail.com")
+puts "We have teams"
